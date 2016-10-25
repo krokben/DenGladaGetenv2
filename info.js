d2a67b66298e4a3370dcdcc21ffe56dd8be1a080
MiesMandoki <miesmandoki@Miess-MacBook-Pro.local>
2016-10-25 12:08:33 +0200

    Ny font ('Raleway')
    Använd denna överallt där vi inte använder ’Lobster’.

--- info.js
+++ info.js
@@ -1,7 +1,7 @@
 function initMap() {
 var denGladeGeten = { lat: 67.86434, lng: 20.21498 };
 var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 8,
+    zoom: 6,
     center: denGladeGeten,
     scrollwheel: false
 
