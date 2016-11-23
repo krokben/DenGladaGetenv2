d2bfa63936fb37c06ab7c929fdad97d4873dd107
Mattias Söderström <mattias.ep@gmail.com>
2016-11-23 15:26:51 +0100

    bytt ut bilder osv

--- admin/bokning.js
+++ admin/bokning.js
@@ -2,3 +2,14 @@ $('.book-item').on('click', function() {
     $('.info').toggle('.hidden');
     $('.click').toggle('.hidden');
 });
+
+
+
+function saySomething(name, age) {
+
+    if(age < 18) {
+        return name + ' is young';
+    }
+
+
+}
