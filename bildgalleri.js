9f21cfd2eb244dc93119602fafcab97ffd2464bf
Natasha George <natashaa.george3@gmail.com>
2016-12-05 16:23:35 +0100

    schå

--- bildgalleri.js
+++ bildgalleri.js
@@ -15,12 +15,11 @@ var Gallery = {
     },
 
     /*Härifråntill--*/bindEvents: function () {
-        this.$mini.on('click', this.showImg);
-        this.$leftbtn.on('click', this.prevImg.bind(this));
-        this.$rightbtn.on('click', this.nextImg.bind(this));                  //Array med bilderna
+        this.$mini.on('click', this.showImg);                   //Array med bilderna
         /*this.$big.on('click', this.hideImg.bind(this));*/     //Fixa så att det går att trycka på knapparna
         this.$big.on('click', this.hideImg.bind(this));
-
+        this.$leftbtn.on('click', this.prevImg.bind(this));
+        this.$rightbtn.on('click', this.nextImg.bind(this));
     },
 
     imgPos: 0,
