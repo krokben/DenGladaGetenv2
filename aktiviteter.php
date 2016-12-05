d15dbae922e63e933bf0a74be29c25bf7845a0be
krokben <tombookpro@TomBook.local>
2016-12-05 14:23:39 +0100

    aktiviteter

--- aktiviteter.php
+++ aktiviteter.php
@@ -20,7 +20,7 @@ $db = mysqli_connect('geten-219508.mysql.binero.se', '219508_rb16043','gladagete
   <link rel='stylesheet' href='main.css'>
   <link rel='stylesheet' href='menu.css'>
   <link rel='stylesheet' href='footer.css'>
-  <link rel='stylesheet' href='info.css'>
+  <link rel='stylesheet' href='aktiviteter.css'>
 </head>
 <body>
     <!-- Header -->
@@ -43,7 +43,7 @@ $db = mysqli_connect('geten-219508.mysql.binero.se', '219508_rb16043','gladagete
                     <li><a id='hem' href='index.php'>Hem</a></li>
                     <li><a id='bildgalleri' href='bildgalleri.php'>Bildgalleri</a></li>
                     <li><a id='bokning' href='bokning.html'>Bokning</a></li>
-                    <li class='active2'><a id='om' href='aktiviteter.html'>Aktiviteter</a></li>
+                    <li class='active2'><a id='om' href='aktiviteter.php'>Aktiviteter</a></li>
                     <li><a id='info1' href='info.html'>Info</a></li>
                 </ul>
             </div>
@@ -54,7 +54,7 @@ $db = mysqli_connect('geten-219508.mysql.binero.se', '219508_rb16043','gladagete
     <div class='row'>
         <div class='col-sm-4 text-center'>
             <br>
-            <img src='images/get.png' class='img-circle' alt='Cinque Terre'>
+            <div class="act-image act-image1"></div>
             <?php
             $query = "
                 SELECT * FROM texts
@@ -66,73 +66,100 @@ $db = mysqli_connect('geten-219508.mysql.binero.se', '219508_rb16043','gladagete
             while($row = mysqli_fetch_assoc($result)) {
                 $heading = $row['rubrik'];
                 $paragraph = $row['text'];
-                echo "<h4 class='rubrik'>$heading</h4><p>$paragraph<br></p>";
+                echo "<h4 class='rubrik'>$heading</h4><div class='act-paragraph'><p>$paragraph<br></p></div>";
                 }
             ?>
         </div>
         <div class='col-sm-4 text-center'>
             <br>
-            <img src='images/geten.png' class='img-circle' alt='Cinque Terre'>
-            <h4 class='rubrik'>Getklappning</h4>
-            <p style='text-align:center;'>
-                <br>
-    Gå in i hagen och klappa getterna! Personal från glada geten följer med och ser
-    till att du kommer nära både Gösta och Selma, och vid rätt tid på året även
-    lammen. Tidsåtgång: ca 30 min
-    Pris: 50 kr per person.
-            </p>
+            <div class="act-image act-image2"></div>
+            <?php
+            $query = "
+                SELECT * FROM texts
+                WHERE id = 3;
+            ";
+
+            $result = mysqli_query($db, $query);
+
+            while($row = mysqli_fetch_assoc($result)) {
+                $heading = $row['rubrik'];
+                $paragraph = $row['text'];
+                echo "<h4 class='rubrik'>$heading</h4><div class='act-paragraph'><p>$paragraph<br></p></div>";
+                }
+            ?>
         </div>
 
         <div class='col-sm-4 text-center'>
             <br>
-            <img src='images/natur.png' class='img-circle' alt='Cinque Terre'>
-            <h4 class='rubrik'>Skogspromenad</h4>
-            <p style='text-align:center;'>
-                <br>
-    Guidad vandring runt området för den som är nyfiken på lite mer lokalkännedom.
-    Tidsåtgång: ca 2 timmar
-    Pris: 250 kr per person.
-    Finns även kartor för kostnadsfria vandringar utan guide.
-            </p>
+            <div class="act-image act-image3"></div>
+            <?php
+            $query = "
+                SELECT * FROM texts
+                WHERE id = 4;
+            ";
+
+            $result = mysqli_query($db, $query);
+
+            while($row = mysqli_fetch_assoc($result)) {
+                $heading = $row['rubrik'];
+                $paragraph = $row['text'];
+                echo "<h4 class='rubrik'>$heading</h4><div class='act-paragraph'><p>$paragraph<br></p></div>";
+                }
+            ?>
         </div>
         <div class='col-sm-4 text-center'>
             <br>
-            <img src='images/spa.png' class='img-circle' alt='Cinque Terre'>
-            <h4 class='rubrik'>SPA</h4>
-            <p style='text-align:center;'>
-                <br>
-                Boka en spaupplevelse på den glada getens egna SPA!
-            </p>
-    <p>
-    Vi erbjuder:
-    Massage 50 min – 350 kr
-    Kurbad 30 min – 250 kr
-    Kroppsscrubb 50 min – 350 kr
-    Badtunna – 300 kr per timme,
-    uppvärmning sköts av den glada geten.</p>
+            <div class="act-image act-image4"></div>
+            <?php
+            $query = "
+                SELECT * FROM texts
+                WHERE id = 5;
+            ";
+
+            $result = mysqli_query($db, $query);
+
+            while($row = mysqli_fetch_assoc($result)) {
+                $heading = $row['rubrik'];
+                $paragraph = $row['text'];
+                echo "<h4 class='rubrik'>$heading</h4><div class='act-paragraph'><p>$paragraph<br></p></div>";
+                }
+            ?>
         </div>
         <div class='col-sm-4 text-center'>
             <br>
-            <img src='images/kalendarium.png' class='img-circle' alt='Cinque Terre'>
-            <h4 class='rubrik'>Kalendarium</h4>
-            <p style='text-align:center;'>
-                <br>
-    December, Julbord – festa loss på den glada getens julbord under hela december månad!
-    Juni, Midsommarfirande.
-            </p>
+            <div class="act-image act-image5"></div>
+            <?php
+            $query = "
+                SELECT * FROM texts
+                WHERE id = 6;
+            ";
+
+            $result = mysqli_query($db, $query);
+
+            while($row = mysqli_fetch_assoc($result)) {
+                $heading = $row['rubrik'];
+                $paragraph = $row['text'];
+                echo "<h4 class='rubrik'>$heading</h4><div class='act-paragraph'><p>$paragraph<br></p></div>";
+                }
+            ?>
         </div>
         <div class='col-sm-4 text-center'>
             <br>
-            <img src='images/skotersafari.png' class='img-circle' alt='Cinque Terre'>
-            <h4 class='rubrik'>Skotersafari</h4>
-            <p style='text-align:center;'>
-    <br>
-    Tidsåtgång 4 timmar
-    pris 500 kr per person
-    du tillsammans med en ledare kan känna att du kör tryggt och säkert på en lagom
-    lång tur i den underbara naturen i tjärnholmens omnejd. Under turerna stannar vi
-    för korvgrillning! Någon form av körkort krävs, svenskt eller utländskt.
-            </p>
+            <div class="act-image act-image6"></div>
+            <?php
+            $query = "
+                SELECT * FROM texts
+                WHERE id = 7;
+            ";
+
+            $result = mysqli_query($db, $query);
+
+            while($row = mysqli_fetch_assoc($result)) {
+                $heading = $row['rubrik'];
+                $paragraph = $row['text'];
+                echo "<h4 class='rubrik'>$heading</h4><div class='act-paragraph'><p>$paragraph<br></p></div>";
+                }
+            ?>
         </div>
     </div>
 
@@ -144,7 +171,7 @@ $db = mysqli_connect('geten-219508.mysql.binero.se', '219508_rb16043','gladagete
                 <li><a href='index.php'>Hem</a></li>
                 <li><a href='bildgalleri.html'>Bildgalleri</a></li>
                 <li><a href='bokning.html'>Bokning</a></li>
-                <li><a href='aktiviteter.html'>Aktiviteter</a></li>
+                <li><a href='aktiviteter.php'>Aktiviteter</a></li>
                 <li class='active'><a href='info.html'>Info</a></li>
                 <li><a href='#'>Kontakt</a></li>
                 <li><a><span class='glyphicon glyphicon-home'></span> Bergmästaregatan 7, 981 33 Kiruna</a></li>
@@ -155,8 +182,5 @@ $db = mysqli_connect('geten-219508.mysql.binero.se', '219508_rb16043','gladagete
                 <li><a href='login.html'>Login</a></li>
             </ul>
         </footer>
-
-<script async defer src='https://maps.googleapis.com/maps/api/js?key=AIzaSyD_STAiy9Xykx7mor4RU2VQDmjf1jrmbXk&callback=initMap'></script>
-<script src='info.js'></script>
 </body>
 </html>
