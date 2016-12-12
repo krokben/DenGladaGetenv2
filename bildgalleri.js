3cd9c48e4a7d9149661d1a95686c361306a2621b
Natasha George <natashaa.george3@gmail.com>
2016-12-12 11:33:18 +0100

    dov

--- bildgalleri.js
+++ bildgalleri.js
@@ -67,15 +67,3 @@ var Gallery = {
     }
 };
 Gallery.init();
-
-
-// event.type must be keypress
-function getChar(event) {
-  if (event.which == null) {
-    return String.fromCharCode(event.keyCode) // IE
-  } else if (event.which!=0 && event.charCode!=0) {
-    return String.fromCharCode(event.which)   // the rest
-  } else {
-    return null // special key
-  }
-}
