7e94309427fe15c4eeb40e404be4e459c1003f53
krokben <tombookpro@TomBook.local>
2016-12-19 10:36:58 +0100

    roomsearch

--- roomsearch.js
+++ roomsearch.js
@@ -22,6 +22,7 @@ $().ready(function() {
 
 // Check if all fields are filled and if confirmation field is equal to email field
 $('input').blur(function() {
+    console.log($('.bookingFieldset').children().find('span').length);
     if($('#datepicker').val() &&
         $('#datepicker2').val() &&
         $('#roomType').val() &&
@@ -29,12 +30,12 @@ $('input').blur(function() {
         $('#lastName').val() &&
         $('#email').val() &&
         $('#bkremail').val() &&
-        $('#email').val() == $('#bkremail').val()) {
-        $('#clicker').prop('disabled', false).val('Bekräfta');
-    }
-    else {
-        $('#clicker').prop('disabled', true).val('Fyll i alla uppgifter');
-    }
+        $('#email').val() == $('#bkremail').val() &&
+        $('.bookingFieldset').children().find('span').length === 0) {
+            $('#clicker').prop('disabled', false).val('Bekräfta');
+        } else {
+            $('#clicker').prop('disabled', true).val('Fyll i alla uppgifter');
+        }
 });
 
 // Error messages
@@ -42,7 +43,7 @@ $('#firstName').focusout(function() {
     if ($(this).val() !== '') { // Only execute if user has put anything in the field
         var pattern = new RegExp(/^[a-zA-Z ]{2,20}$/);
         if (pattern.test($(this).val()) === false) {
-            $('#firstNameLabel').append(' <span class="label label-danger">Endast 2 - 20 bokstäver tillåtna.</span>').addClass('error_message').val('');
+            $('#firstNameLabel').append(' <span class="label label-danger">Endast 2 - 20 bokstäver tillåtna.</span>');
         }
     }
 });
@@ -58,13 +59,44 @@ $('#lastName').focusout(function() {
         var pattern = new RegExp(/^[a-zA-Z ]{2,20}$/);
         if (pattern.test($(this).val()) === false) {
             console.log($(this).prev());
-            $('#lastNameLabel').append(' <span class="label label-danger">Endast 2 - 20 bokstäver tillåtna.</span>').addClass('error_message').val('');
+            $('#lastNameLabel').append(' <span class="label label-danger">Endast 2 - 20 bokstäver tillåtna.</span>');
         }
     }
 });
 
 $('#lastName').focus(function() {
     if ($('#lastNameLabel').html().length > 9) {
-        $('#lastNameLabel').html($('#firstNameLabel').html().slice(0, 9)); // Remove alert on focus
+        $('#lastNameLabel').html($('#lastNameLabel').html().slice(0, 9)); // Remove alert on focus
+    }
+});
+
+$('#email').focusout(function() {
+    if ($(this).val() !== '') { // Only execute if user has put anything in the field
+        var pattern = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+        if (pattern.test($(this).val()) === false) {
+            console.log($(this).prev());
+            $('#emailLabel').append(' <span class="label label-danger">Fyll i en giltig mailadress.</span>');
+        }
+    }
+});
+
+$('#email').focus(function() {
+    if ($('#emailLabel').html().length > 10) {
+        $('#emailLabel').html($('#emailLabel').html().slice(0, 10)); // Remove alert on focus
+    }
+});
+
+$('#bkremail').focusout(function() {
+    if ($(this).val() !== '') { // Only execute if user has put anything in the field
+        console.log($(this).val());
+        if ($(this).val() !== $('#email').val()) {
+            $('#confirmEmailLabel').append(' <span class="label label-danger">Fältet måste matcha det ovan.</span>');
+        }
+    }
+});
+
+$('#bkremail').focus(function() {
+    if ($('#confirmEmailLabel').html().length > 19) {
+        $('#confirmEmailLabel').html($('#confirmEmailLabel').html().slice(0, 19)); // Remove alert on focus
     }
 });
