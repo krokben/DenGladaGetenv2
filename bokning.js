192f504afc5c729c4c0f1077296fdd8a0c666035
krokben <tombookpro@TomBook.local>
2016-12-19 15:54:29 +0100

    gitt

--- bokning.js
+++ bokning.js
@@ -1,26 +1,26 @@
 $().ready(function() {
-    $('#clicker').click(function() {
-        $('input:not(:last-child)').each(function() {
-            if ($(this).attr('disabled')) {
-                $(this).removeAttr('disabled');
-            }
-            else {
-                $(this).attr({
-                    'disabled': 'disabled'
-                });
-            }
-        });
-        $('select').each(function() {
-            if ($(this).attr('disabled')) {
-                $(this).removeAttr('disabled');
-            }
-            else {
-                $(this).attr({
-                    'disabled': 'disabled'
-                });
-            }
-        });
-    });
+    // $('#clicker').click(function() {
+    //     $('input:not(:last-child)').each(function() {
+    //         if ($(this).attr('disabled')) {
+    //             $(this).removeAttr('disabled');
+    //         }
+    //         else {
+    //             $(this).attr({
+    //                 'disabled': 'disabled'
+    //             });
+    //         }
+    //     });
+    //     $('select').each(function() {
+    //         if ($(this).attr('disabled')) {
+    //             $(this).removeAttr('disabled');
+    //         }
+    //         else {
+    //             $(this).attr({
+    //                 'disabled': 'disabled'
+    //             });
+    //         }
+    //     });
+    // });
 
 // Datepicker
 
--- roomsearch.js
+++ roomsearch.js
@@ -4,15 +4,20 @@ $().ready(function() {
         console.log($(this).parent().parent().find('input[type=text]').val());
         $('#roomType').val($(this).parent().parent().find('input[type=text]').val());
     switch ($('#roomType').val()) {
-        case 'Enkel':   $('.enkel').parent().css('background', '#39B647');
+        case '1':
+        case '2':   $('.enkel').parent().css('background', '#39B647');
                         $('.dubbel').parent().css('background', '#F1F1F1');
                         $('.familje').parent().css('background', '#F1F1F1');
                         break;
-        case 'Dubbel':  $('.dubbel').parent().css('background', '#39B647');
+        case '3':
+        case '4':
+        case '5':
+        case '6':  $('.dubbel').parent().css('background', '#39B647');
                         $('.enkel').parent().css('background', '#F1F1F1');
                         $('.familje').parent().css('background', '#F1F1F1');
                         break;
-        case 'Familje': $('.familje').parent().css('background', '#39B647');
+        case '7':
+        case '8': $('.familje').parent().css('background', '#39B647');
                         $('.dubbel').parent().css('background', '#F1F1F1');
                         $('.enkel').parent().css('background', '#F1F1F1');
                         break;
